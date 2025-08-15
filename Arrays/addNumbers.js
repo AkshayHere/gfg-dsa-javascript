06ec5528710832191e0472dd77325f7c1f3978a7
Akshay <akshay.mohan@hlas.com.sg>
2025-08-15 12:04:31 +0800

    - Restructure folders - added gitignore

---
+++ Arrays/addNumbers.js
@@ -0,0 +1,30 @@
+/**
+ * Simple example to understand recursive behavior
+ */
+
+const addNumbers = () => {
+    let output = 0;
+    const numbers = [0, 1, 2, 3, 4, 5];
+    console.log('numbers: ', numbers);
+    console.log('numbers.length: ', numbers.length);
+    for(i = 0; i < numbers.length; i++){
+        output = output + numbers[i];
+    }
+    console.log('output: ', output);
+}
+
+const addNumbersRecursive = (numbers, index, output) => {
+    if(index < numbers.length){
+        console.log('index >> : ', index);
+        output = output + numbers[index];
+        console.log('output: ', output);
+        // index++; // This increments by 1
+        // index+=1; // This increments by 1.
+        ++index; // This increments by 2
+        return addNumbersRecursive(numbers, index, output);
+    }
+    console.log('output: ', output);
+    return output;
+}
+
+addNumbersRecursive([0, 1, 2, 3, 4, 5], 0, 0);
